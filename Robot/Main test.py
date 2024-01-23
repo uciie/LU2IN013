757e42a66e4560c0d019ff129f58d416e473abfa
Scyyt <eric11338634@gmail.com>
2024-01-23 16:20:57 +0100

    test déplacement

--- Robot/Main test.py
+++ Robot/Main test.py
@@ -5,9 +5,14 @@ from Grille import *
 
 def main():
     g = Grille(5,5)
-    r = Robot("Dexter", 1, 1, 1, 1, g)
+    r = Robot("Dexter", 2, 2, 1, 1, g)
     
+    print("Etat initial")
     g.addRobot(r)
+    g.affiche()
+
+    print("Etat final")
+    r.go(2)
     g.affiche()
 
 main()
