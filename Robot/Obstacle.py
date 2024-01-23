2c2b48cf1c774c70733cf970ad44a6935437760b
Michaël LI <michaelli2004@hotmail.fr>
2024-01-23 16:58:00 +0100

    commit

--- Robot/Obstacle.py
+++ Robot/Obstacle.py
@@ -1,4 +1,7 @@
 # @autors equipe HELMS
-
+from Grille import Grille
 class Obstacle:
-  def __init__(self):
+  def __init__(self, posX, posY, longueur, largeur, grille):
+    for i in range(posX, largeur):
+      for y in range(posY, longueur):
+        
