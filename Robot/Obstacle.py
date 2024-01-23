101a182bd25328fdd88c551f2b8ae2a769d00013
Michaël LI <michaelli2004@hotmail.fr>
2024-01-23 17:00:09 +0100

    commit

--- Robot/Obstacle.py
+++ Robot/Obstacle.py
@@ -4,4 +4,5 @@ class Obstacle:
   def __init__(self, posX, posY, longueur, largeur, grille):
     for i in range(posX, largeur):
       for y in range(posY, longueur):
-        
+        grille.addObstacle(1, i, y)
+      grille.addObstacle(1,i,y)
