3d0779967a372e34c49d22fd3ac40d2793886979
Scyyt <eric11338634@gmail.com>
2024-01-23 18:58:58 +0100

    test de bouger le robot

--- Robot/main test v2.py
+++ Robot/main test v2.py
@@ -4,6 +4,17 @@ from Obstacle import *
 
 
 def main():
-    g = Grille(9,9)
+    g = Grille(5,5)
     r = Robot("Dexter", 2, 2, 1, 1, g)
     o = Obstacle(2,1,0,0,g)
+
+    print("Etat initial")
+    g.affiche()
+
+    for i in range(2):
+        r.move_one("UP", 0)
+        r.move_one("RIGHT", 0)
+        print(str(i+1) + "e itération")
+        g.affiche()
+
+main()
