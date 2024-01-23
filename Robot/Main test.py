942897916b267e0f15c30d8b6a1412ffc5b19d44
Michaël LI <michaelli2004@hotmail.fr>
2024-01-23 17:56:22 +0100

    commit

--- Robot/Main test.py
+++ Robot/Main test.py
@@ -1,12 +1,13 @@
 from Robot import *
 from Grille import *
-
+from Obstacle import *
 
 
 def main():
     g = Grille(5,5)
     r = Robot("Dexter", 2, 2, 1, 1, g)
-    
+    o = Obstacle(0,3,1,1,g)
+    o.addObstacle()
     try:
         print("Etat initial")
         g.addRobot(r)
