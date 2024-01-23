88aa19c4b6a80e60be64d17efbf71adc49573ae8
Michaël LI <michaelli2004@hotmail.fr>
2024-01-23 17:03:00 +0100

    commit

--- Robot/Obstacle.py
+++ Robot/Obstacle.py
@@ -2,6 +2,13 @@
 from Grille import Grille
 class Obstacle:
   def __init__(self, posX, posY, longueur, largeur, grille):
+    self.posX = posX
+    self.posY = posY
+    self.longueur = longueur
+    self.largeur = largeur
+    self.grille = grille
+  
+  def addObstacle(self):
     for i in range(posX, largeur):
       for y in range(posY, longueur):
         grille.addObstacle(1, i, y)
