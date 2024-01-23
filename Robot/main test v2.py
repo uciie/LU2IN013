f71847bd704a3d70412e6970503e1e358b2deb41
Scyyt <eric11338634@gmail.com>
2024-01-23 18:30:20 +0100

    création main test2

--- Robot/main test v2.py
+++ Robot/main test v2.py
@@ -3,19 +3,3 @@ from Grille_v2_no_fini import *
 
 
 def main():
-    g = Grille(5,5)
-    r = Robot("Dexter", 2, 2, 1, 1, g)
-    
-    try:
-        print("Etat initial")
-        g.addRobot(r)
-        g.affiche()
-
-        print("Etat final")
-        r.go(10)
-        g.affiche() 
-    except IndexError as e :
-        pass
-    
-    
-main()
