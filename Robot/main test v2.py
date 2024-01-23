dabaee70afb99c12ae3dd0daa6e14dcc3318366a
Scyyt <eric11338634@gmail.com>
2024-01-23 18:50:44 +0100

    initialisation entité

--- Robot/main test v2.py
+++ Robot/main test v2.py
@@ -1,5 +1,9 @@
 from Robot_v2_no_fini import *
 from Grille_v2_no_fini import *
+from Obstacle import *
 
 
 def main():
+    g = Grille(9,9)
+    r = Robot("Dexter", 2, 2, 1, 1, g)
+    o = Obstacle(2,1,0,0,g)
