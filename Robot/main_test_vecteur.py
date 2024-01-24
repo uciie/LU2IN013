06c8452862769a6a3e5e08fd4e663ed44e51939e
uciie <pan.luciie@gmail.com>
2024-01-24 20:05:07 +0100

    reorganisation de la branche

--- Robot/main_test_vecteur.py
+++ Robot/main_test_vecteur.py
@@ -5,8 +5,24 @@ def main():
     """
     Soit deux point x, y ayant comme coord (x_a,x_b) et (x_b,y_b)
     """
-    x_a, y_a = 0.0,0.0
-    y_a, y_b= 2.0,2.5
-    v1 = Vecteur("vect_dir", xa, ya, xb, yb)
+    x_b, y_b= 2.0,2.5
+    v1 = Vecteur("vect_dir", (x_b, y_b))
+    v2 = Vecteur("vect_dir", (x_b-2, y_b+1))
+
+    print(v1.nom)
+    print("Composantes : " + str (v1.composantes))
+    print("Norme : " + str(v1.norme))
+
+    print(v2.nom)
+    print("Composantes : " + str (v2.composantes))
+    print("Norme : " + str(v2.norme))
+
+    v3 = v1.add(v2)
+    print(v3.nom)
+    print("Composantes : " + str (v1.composantes))
+    print("Norme : " + str(v1.norme))
+    print()
+    
+main()
 
     
