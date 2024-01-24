736167bbbd21322faf4767fd31a8a034940282f6
Michaël LI <michaelli2004@hotmail.fr>
2024-01-24 17:51:26 +0100

    commit

--- Robot/Vecteur.py
+++ Robot/Vecteur.py
@@ -1,2 +1,14 @@
 class Vecteur():
-    def __init__(xa, ya, xb, yb):
+    def __init__(self, nom, xa, ya, xb, yb):
+        """string nom float xa float ya float xb float yb -> vecteur
+        Initialisation d'un vecteur"""
+        
+        # Nom du vecteur
+        self.nom = nom
+
+        # Coordonnées du premier point
+        self.xa = xa
+        self.ya = ya
+
+        # Coordonées du second point
+        
