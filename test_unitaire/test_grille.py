419103d1225122952e90db1a180e8d6253f6b476
Michaël LI <michaelli2004@hotmail.fr>
2024-02-04 16:21:42 +0100

    test_grille

--- test_unitaire/test_grille.py
+++ test_unitaire/test_grille.py
@@ -6,8 +6,9 @@ class TestGrille(unittest.TestCase) :
     def setUp(self):
         """Initialiasation de la grille
         """
-        self.g = Grille(20, 20, 1)
+        self.g = Grille(5, 5, 1)
 
-    def test_affiche():
+    def test_affiche(self):
         """Teste l'affichage
         """
+        self.g.affiche()
