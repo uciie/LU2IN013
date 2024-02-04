47846b4f279f35b2bb7834585cbec7e1f8b34c05
Michaël LI <michaelli2004@hotmail.fr>
2024-02-04 15:35:09 +0100

    test_robot

--- test_unitaire/test_robot.py
+++ test_unitaire/test_robot.py
@@ -1,14 +1,23 @@
 import unittest
-from .simulation/robot import Robot
+from simulation import Robot
 
 class TestRobot(unittest.TestCase):
 
-    def setUp(self) :
+    def setUp(self):
         """Initialisation du robot"""
         r = Robot("r", 5, 5, 2, 2, "red")
     
     def test_getCurrPos(self):
         """Teste les coordonées actuelles du robot
         """
-        resultat_pos = getCurrPos(r)
+        resultat_pos = self.r.getCurrPos()
         self.assertEqual(resultat_pos.x, (r.posX, r.posY))
+    
+    def test_move_dOm(self):
+        """Teste le mouvement du robot
+        """
+        mouvement = (r.posX + 1, r.posY + 1)
+        resultat_move = self.r.move_dOM(1,1)
+        self.assertEqual((r.posX, r.posY), mouvement)
+
+    
