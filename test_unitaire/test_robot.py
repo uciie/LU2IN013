d51477a95d9fb7303e7ddd438d641b942dc9f193
Michaël LI <michaelli2004@hotmail.fr>
2024-02-04 15:19:23 +0100

    test_robot

--- test_unitaire/test_robot.py
+++ test_unitaire/test_robot.py
@@ -1,1 +1,8 @@
-from simulation.robot import Robot
+import unittest
+from .simulation/robot import Robot
+
+class TestRobot(unittest.TestCase):
+
+    def setUp(self) :
+        """Initialisation du robot"""
+        r = Robot("r", 5, 5, 2, 2, "red")
