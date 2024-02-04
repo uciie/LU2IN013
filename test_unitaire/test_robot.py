99cc9d7576d7d15f2f45d83098146bc81d247529
Michaël LI <michaelli2004@hotmail.fr>
2024-02-04 15:56:34 +0100

    test_robot

--- test_unitaire/test_robot.py
+++ test_unitaire/test_robot.py
@@ -1,17 +1,17 @@
 import unittest
-from simulation import Robot
+from simulation.robot import Robot
 
 class TestRobot(unittest.TestCase):
 
     def setUp(self):
         """Initialisation du robot"""
-        r = Robot("r", 5, 5, 2, 2, "red")
+        self.r = Robot("r", 5, 5 ,2, 2, (1,1) , "red")
     
     def test_getCurrPos(self):
         """Teste les coordonnées actuelles du robot
         """
         resultat_pos = self.r.getCurrPos()
-        self.assertEqual(resultat_pos.x, (self.r.posX, self.r.posY))
+        self.assertEqual(resultat_pos, (self.r.posX, self.r.posY))
     
     def test_move_dOm(self):
         """Teste le mouvement du robot
@@ -23,7 +23,7 @@ class TestRobot(unittest.TestCase):
     def test_getLasPos(self):
         """Teste les anciennes coordonnées du robot
         """
-        ancienne_pos = (self.r.posX; self.r.posY)
+        ancienne_pos = (self.r.posX, self.r.posY)
         self.r.move_dOM(1,1)
         resultat_getLastPos = self.r.getLastPos()
         self.assertEqual(resultat_getLastPos, ancienne_pos)
