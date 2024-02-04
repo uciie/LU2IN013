d7dbd259ae8cf819c6a4869614bf585a85dbc43c
Michaël LI <michaelli2004@hotmail.fr>
2024-02-04 15:22:57 +0100

    test_robot

--- test_unitaire/test_robot.py
+++ test_unitaire/test_robot.py
@@ -6,3 +6,9 @@ class TestRobot(unittest.TestCase):
     def setUp(self) :
         """Initialisation du robot"""
         r = Robot("r", 5, 5, 2, 2, "red")
+    
+    def test_getCurrPos(self):
+        """Teste les coordonées actuelles du robot
+        """
+        resultat_pos = getCurrPos(r)
+        self.assertEqual(resultat_pos.x, (r.posX, r.posY))
