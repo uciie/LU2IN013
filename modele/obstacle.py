6f065fb32f7e3ac4986e30131cccf908f8fdf75f
Scyyt <eric11338634@gmail.com>
2024-02-18 17:18:35 +0100

    ajout commentaire + vecteurs directeurs

--- modele/obstacle.py
+++ modele/obstacle.py
@@ -1,5 +1,7 @@
+from .vecteur import Vecteur
+
 class Obstacle():
-    def __init__(self, posX : float, posY :float, length : float, width : float):
+    def __init__(self, posX : float, posY :float, length : float, width : float, v1 : Vecteur, v2 : Vecteur, v3 : Vecteur, v4 : Vecteur):
         """ Initialise un obstacle
 
         :param posX: Coordonnée X de l'obstacle (float)
@@ -8,7 +10,16 @@ class Obstacle():
         :param width: Largeur de l'obstacle (float)
         """
 
+        # Coordonnées du centre de l'obstacle
         self.posX = posX
         self.posY = posY
+
+        # Dimensions du robot
         self.length = length
         self.width = width
+
+        # Vecteurs directeurs de l'obstacle
+        self.v1 = v1
+        self.v2 = v2
+        self.v3 = v3
+        self.v4 = v4
