59fe13e6ef9b9bdafcd1ff17b8030e802bd61852
Scyyt <eric11338634@gmail.com>
2024-02-18 15:51:30 +0100

    docstring obstacle

--- modele/obstacle.py
+++ modele/obstacle.py
@@ -1,6 +1,12 @@
 class Obstacle():
     def __init__(self, posX : float, posY :float, length : float, width : float):
-    
+        """ Initialise un obstacle
+
+        :param posX: Coordonnée X de l'obstacle (float)
+        :param posY: Coordonnée y de l'obstacle (float)
+        :param length: Longueur de l'obstacle (float)
+        :param width: Largeur de l'obstacle (float)
+        """
 
         self.posX = posX
         self.posY = posY
