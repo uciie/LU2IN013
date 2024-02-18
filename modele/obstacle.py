1f741cf0a627e2c2b27b754a0709c1d9d63bf466
Scyyt <eric11338634@gmail.com>
2024-02-18 15:46:33 +0100

    init class obstacle

---
+++ modele/obstacle.py
@@ -0,0 +1,8 @@
+class Obstacle():
+    def __init__(self, posX : float, posY :float, length : float, width : float):
+    
+
+        self.posX = posX
+        self.posY = posY
+        self.length = length
+        self.width = width
