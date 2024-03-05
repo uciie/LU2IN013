384f299267af80636063dcbc16697ec11290bbb9
uciie <pan.luciie@gmail.com>
2024-03-05 23:06:54 +0100

    renommage des noms de variables

--- MVC/modele/obstacle.py
+++ MVC/modele/obstacle.py
@@ -1,7 +1,7 @@
 from .vecteur import Vecteur
 
 class Obstacle:
-    def __init__(self, posX: float, posY: float, v1: Vecteur, v2: Vecteur, v3: Vecteur, v4: Vecteur, color: str):
+    def __init__(self, posX: float, posY: float, coin1: Vecteur, coin2: Vecteur, coin3: Vecteur, coin4: Vecteur, color: str):
         """ Initialise un obstacle
 
         :param posX: Coordonnée X de l'obstacle (float)
@@ -19,10 +19,10 @@ class Obstacle:
         self._posY = posY
 
         # Vecteurs directeurs de l'obstacle
-        self._v1 = v1
-        self._v2 = v2
-        self._v3 = v3
-        self._v4 = v4
+        self._coin1 = coin1
+        self._coin2 = coin2
+        self._coin3 = coin3
+        self._coin4 = coin4
 
     @property
     def posX(self) -> float:
@@ -40,31 +40,31 @@ class Obstacle:
         return self._color
 
     @property
-    def v1(self) -> Vecteur:
+    def coin1(self) -> Vecteur:
         """ Propriété pour l'attribut v1 """
-        return self._v1
+        return self._coin1
 
     @property
-    def v2(self) -> Vecteur:
+    def coin2(self) -> Vecteur:
         """ Propriété pour l'attribut v2 """
-        return self._v2
+        return self._coin2
 
     @property
-    def v3(self) -> Vecteur:
+    def coin3(self) -> Vecteur:
         """ Propriété pour l'attribut v3 """
-        return self._v3
+        return self._coin3
 
     @property
-    def v4(self) -> Vecteur:
+    def coin4(self) -> Vecteur:
         """ Propriété pour l'attribut v4 """
-        return self._v4
+        return self._coin4
 
     @property
     def coins(self):
         """ Renvoie les coordonnées des 4 coins de l'obstacle """
-        x1, y1 = self._posX + self._v1.x / 2, self._posY - self._v2.y / 2
-        x2, y2 = self._posX + self._v1.x / 2, self._posY + self._v2.y / 2
-        x3, y3 = self._posX - self._v1.x / 2, self._posY + self._v2.y / 2
-        x4, y4 = self._posX - self._v1.x / 2, self._posY - self._v2.y / 2
+        x1, y1 = self._posX + self._coin1.x / 2, self._posY - self._coin2.y / 2
+        x2, y2 = self._posX + self._coin1.x / 2, self._posY + self._coin2.y / 2
+        x3, y3 = self._posX - self._coin1.x / 2, self._posY + self._coin2.y / 2
+        x4, y4 = self._posX - self._coin1.x / 2, self._posY - self._coin2.y / 2
 
         return [x1, y1, x2, y2, x3, y3, x4, y4]
