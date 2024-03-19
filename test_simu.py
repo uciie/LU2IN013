656cd6a75a3cc3cf0a0d268dedc7e9a0d3fc8905
uciie <pan.luciie@gmail.com>
2024-03-19 23:06:03 +0100

    suppression des __pycache__

--- test_simu.py
+++ test_simu.py
@@ -17,7 +17,7 @@ def main():
     # Initialisation de l'arene, robot, obstacle
     arene = Arene("Simulation de déplacement du robot", largeur, hauteur, echelle)
     robot = SimuRobot("R", int(largeur / 2), int(hauteur / 2), dim_robot_x, dim_robot_y, 10, 150, color="red")
-    obs = ObstacleRectangle(200, 200, Vecteur(10, 0), Vecteur(0, 20), Vecteur(10, 0), Vecteur(0, 20), color="blue")
+    obs = ObstacleRectangle(200, 200, Vecteur(10, 0), Vecteur(0, 20), color="blue")
 
     # Ajouter un obstacle dans l'arene
     arene.add_obstacle(obs)
@@ -26,7 +26,7 @@ def main():
     simu = Simulation("Simulation", dt_controller, robot, arene, lock_aff)
 
     # donner une vitesse au robot
-    robot.set_vitesse_roue(50, 50)
+    robot.set_vitesse_roue(0, 50)
 
     # demarer la simulation
     simu.run()
