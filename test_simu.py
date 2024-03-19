42984d3a0f87524cb686a8041399405cfb0885b3
uciie <pan.luciie@gmail.com>
2024-03-19 23:47:45 +0100

    test d'une modelisation qu'avec l'environnement et le robot (ni de controleur ni de view)

---
+++ test_simu.py
@@ -0,0 +1,36 @@
+import threading
+
+from MVC.modele.vecteur import Vecteur
+from MVC.modele.simulation import Simulation
+from MVC.modele.objets import SimuRobot, Arene, ObstacleRectangle
+
+
+def main():
+    # Création du verrou
+    lock_aff = threading.RLock()
+
+    dt_controller = 1/100
+    echelle = 5
+    largeur, hauteur = 500, 500
+    dim_robot_x, dim_robot_y = int(largeur / 10), int(hauteur / 10)
+
+    # Initialisation de l'arene, robot, obstacle
+    arene = Arene("Simulation de déplacement du robot", largeur, hauteur, echelle)
+    robot = SimuRobot("R", int(largeur / 2), int(hauteur / 2), dim_robot_x, dim_robot_y, 10, 150, color="red")
+    obs = ObstacleRectangle(200, 200, Vecteur(10, 0), Vecteur(0, 20), color="blue")
+
+    # Ajouter un obstacle dans l'arene
+    arene.add_obstacle(obs)
+
+    # Créer la simulation
+    simu = Simulation("Simulation", dt_controller, robot, arene, lock_aff)
+
+    # donner une vitesse au robot
+    robot.set_vitesse_roue(0, 50)
+
+    # demarer la simulation
+    simu.run()
+
+
+if __name__ == '__main__':
+    main()
