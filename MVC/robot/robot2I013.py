364faf54e7d3588d96bda9ff575929b26ab415e9
PraiseTheLemon <michaelli2004@hotmail.fr>
2024-03-26 18:50:38 +0100

    ajout de la valeur en dehors l'intervalle

--- MVC/robot/robot2I013.py
+++ MVC/robot/robot2I013.py
@@ -78,6 +78,8 @@ class Robot2IN013:
             1. L'intervalle est de **5-8,000** millimeters.
             2. Lorsque la valeur est en dehors de l'intervalle, le retour est **8190**.
         """
+        if random() > 0.5:
+            return 8190
         return random(5, 8000)
 
     def servo_rotate(self, position):
