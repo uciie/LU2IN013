789378fc7b847b7445faa271744b0535b2c84dd7
uciie <pan.luciie@gmail.com>
2024-03-26 20:28:23 +0100

    correction du commit precedent

--- MVC/controller/adaptateur_robot_irl.py
+++ MVC/controller/adaptateur_robot_irl.py
@@ -1,215 +1,108 @@
-import logging
 import math
 
-from ..controller.controleur import Adaptateur, Strategie
-
-# Configure logging
-logging.basicConfig(level=logging.DEBUG, filename='ai.log', filemode='w',
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+from MVC.controller.controleur import Adaptateur
+from MVC.robot.robot2I013 import Robot2IN013
 
 
-# Désactiver les messages de journalisation pour le module spécifié
-# logging.getLogger('MVC.controller.ai').setLevel(logging.WARNING)
+class AdaptateurRobotIrl(Adaptateur):
+    """Classe de l'adaptateur du robot irl"""
 
-class Go(Strategie):
-    """ Classe responsable d'avancer le robot """
-
-    def __init__(self, adaptateur: Adaptateur, distance: float, v_ang_d: float, v_ang_g: float,
-                 active_trace: bool = False) -> None:
-        """
-        :param adaptateur: l'adaptateur du robot
-        :param distance: la distance à parcourir robot
-        :param v_ang_d: la vitesse angulaire droite du robot
-        :param v_ang_g: la vitesse angulaire gauche du robot
-        :param active_trace: activation du tracage du parcours du robot
+    def __init__(self, robot: Robot2IN013):
+        """ Adaptateur du robot irl
         """
         super().__init__()
-        self.adaptateur = adaptateur
-        self.pos_ini = None
-        self.distance = distance
-        self.v_ang_d, self.v_ang_g = v_ang_d, v_ang_g
-        self.parcouru = 0.
-        self.active_trace = active_trace
-        self.logger = logging.getLogger(__name__)
+        self._robot = robot
+        self._v_ang_roue_d, self._v_ang_roue_g = 0., 0.
 
-    def start(self):
-        """Commencer la strategie"""
-        self.logger.info("Starting Go strategy")
-        self.adaptateur.active_trace(self.active_trace)
-        self.adaptateur.set_vitesse_roue(self.v_ang_d, self.v_ang_g)
-        self.pos_ini = self.adaptateur.distance_parcourue
-        self.parcouru = 0.
+    @property
+    def v_ang_d(self) -> float:
+        return self._v_ang_roue_d
+
+    @v_ang_d.setter
+    def v_ang_d(self, v_ang_roue_d: float):
+        self._v_ang_roue_d = v_ang_roue_d
+
+    @property
+    def v_ang_g(self) -> float:
+        return self._v_ang_roue_g
+
+    @v_ang_g.setter
+    def v_ang_g(self, v_ang_roue_g: float):
+        self._v_ang_roue_g = v_ang_roue_g
+
+    @property
+    def robot(self) -> Robot2IN013:
+        return self._robot
+
+    def set_vitesse_roue(self, v_ang_roue_d: float, v_ang_roue_g: float):
+        """ Modifier la vitesse des roues
+
+        :param v_ang_roue_d: Modifier la vitesse angulaire de la roue droite
+        :param v_ang_roue_g: Modifier la vitesse angulaire de la roue gauche
+        """
+        self._v_ang_roue_d = v_ang_roue_d
+        self._v_ang_roue_g = v_ang_roue_g
+        self._robot.set_motor_dps("roue_droite", v_ang_roue_d)
+        self._robot.set_motor_dps("roue_gauche", v_ang_roue_g)
+
+    @property
+    def distance_parcourue(self) -> float:
+        """ Obtenir la distance parcourue
+        :returns : Renvoie la distance parcourue du robot
+        """
+        return self.angle_parcourue / 360 * self._robot.WHEEL_DIAMETER * 2 * math.pi
+
+    @property
+    def angle_parcourue(self) -> float:
+        """ Obtenir l'angle parcouru
+        :returns : Renvoie l'angle parcourut du robot en degree
+        """
+        # obtenir la position des angles des roues
+        pos_roues_x, pos_roues_y = self._robot.get_motor_position()
+        # moyenne d'angle parcourue
+        angle_parcourue = (pos_roues_x + pos_roues_y) / 2
+
+        self._robot.offset_motor_encoder("roue_droite", 0)
+        self._robot.offset_motor_encoder("roue_gauche", 0)
+        #angle_parcourue = 1
+        return angle_parcourue
 
     def stop(self):
-        """Verifier si la strategie est fini ou non
-        :return: True si la strategie est finie, False sinon"""
-        self.logger.info(f"distance parcourue {self.parcouru}, distance {self.distance}")
-        # v_roue_d, v_roue_g = self.adaptateur.vitesse_ang_roues
-        return math.fabs(self.parcouru) >= math.fabs(self.distance)
-
-    def step(self):
-        """pas de la strategie """
-        self.parcouru += self.adaptateur.distance_parcourue
-        if self.stop():
-            self.adaptateur.stop()
-            self.logger.info("Go strategy finished")
-            return
-
-
-class TournerDeg(Strategie):
-    """Classe qui permet de tourner le robot d'un certain degree"""
-
-    def __init__(self, adaptateur: Adaptateur, angle: float, v_ang: float, active_trace: bool = False) -> None:
+        """ Arreter le robot irl
         """
-        :param adaptateur: l'adaptateur du robot
-        :param angle: l'angle à parcourir robot
-        :param v_ang: la vitesse angulaire du robot
-        :param active_trace: activation du tracage du parcours du robot
+        self.set_vitesse_roue(0, 0)
+
+    def actualiser(self):
+        """mettre à jour """
+        self.set_vitesse_roue(self._v_ang_roue_d, self._v_ang_roue_g)
+
+    @property
+    def vitesse_ang_roues(self) -> tuple[float, float]:
+        """ Obtenir la vitesse angulaire des roues droite et gauche
+        :return: la vitesse angulaire des roues
         """
-        super().__init__()
-        self.pos_ini = None
-        self.adaptateur = adaptateur
-        self.angle, self.v_ang = angle, v_ang
-        self.parcouru = 0.
-        self.active_trace = active_trace
-        self.logger = logging.getLogger(__name__)
+        return self._v_ang_roue_d, self._v_ang_roue_g
 
-    def start(self):
-        """Commencer la strategie"""
-        self.logger.info("Starting TournerDeg strategy")
-        self.adaptateur.active_trace(self.active_trace)
-        if self.angle > 0:
-            self.v_ang_d, self.v_ang_g = self.v_ang, -self.v_ang
-            self.adaptateur.set_vitesse_roue(self.v_ang, -self.v_ang)
-        else:
-            self.v_ang_d, self.v_ang_g = -self.v_ang, self.v_ang
-            self.adaptateur.set_vitesse_roue(-self.v_ang, self.v_ang)
-        self.pos_ini = 1. * self.adaptateur.angle_parcourue
-        self.parcouru = 0.
-
-    def stop(self) -> bool:
-        """Verifier si la strategie est fini ou non
-        :return: True si la strategie est finie ou False sinon"""
-        self.logger.info(f"angle parcourue {self.parcouru}, angle {self.angle}")
-        return math.fabs(self.parcouru) >= math.fabs(self.angle)
-
-    def step(self):
-        """pas de la strategie"""
-        self.parcouru += self.adaptateur.angle_parcourue
-        if self.stop():
-            self.adaptateur.stop()
-            self.logger.info("TournerDeg strategy finished")
-            return
-
-
-class StrategieSequentielle(Strategie):
-    """Classe Strategie sequentielle permettant de faire des combinaisons de la strategie basique"""
-
-    def __init__(self, adaptateur: Adaptateur, steps: list[Strategie]) -> None:
+    @property
+    def info(self) -> str:
+        """ afficher les informations du robot
+        :return str: informations du robot
         """
-        :param adaptateur: adaptateur du robot
-        :param steps: liste des steps
-        """
-        super().__init__()
-        self.adaptateur = adaptateur
-        self.pos_ini = None
-        self.parcouru = 0.
-        self.steps = steps
-        self.current_step = 0
+        info_str = ""
+        info_str += f"Vitesse roue droite: {self._v_ang_roue_d}\n"
+        info_str += f"Vitesse roue gauche: {self._v_ang_roue_g}\n"
+        return info_str
 
-        self.logger = logging.getLogger(__name__)
-
-    def start(self):
-        """commencer la strategie"""
-        self.logger.info("Starting Sequential strategy")
-        self.current_step = 0
-        self.steps[self.current_step].start()
-
-    def stop(self) -> bool:
-        """Verifier si la strategie est finie
-        :return: True si la strategie est finie, False sinon"""
-        return self.current_step >= len(self.steps) or self.steps[self.current_step].stop()
-
-    def step(self):
-        """pas de la strategie sequentielle """
-        if self.stop():
-            return
-        self.steps[self.current_step].step()
-        if self.steps[self.current_step].stop():
-            self.current_step += 1
-            if self.current_step < len(self.steps):
-                self.steps[self.current_step].start()
-
-
-class Stop(Strategie):
-    """Strategie qui ne fait rien/ arrete le robot"""
-
-    def __init__(self, adaptateur: Adaptateur):
-        """
-        :param adaptateur: adaptateur du robot
-        """
-        super().__init__()
-        self.adaptateur = adaptateur
-        self.logger = logging.getLogger(__name__)
-
-    def start(self):
-        """commencer la strategie"""
-        self.logger.info("Starting STOP strategy")
-        self.adaptateur.set_vitesse_roue(0, 0)
-
-    def stop(self) -> bool:
-        """Verifier si la strategie est finie
-        :return: True si la strategie est finie, False sinon"""
-        return True
-
-    def step(self):
+    def active_trace(self, val: bool):
         pass
 
-
-class StrategieIf(Strategie):
-    """Classe Strategie conditionnelle faire strat_a si condition verifiée, sinon strat_b"""
-
-    def __init__(self, adaptateur: Adaptateur, strat_a: Strategie, strat_b: Strategie, condition: float) -> None:
+    @property
+    def get_distance(self) -> float:
         """
-        :param adaptateur: adaptateur du robot
-        :param strat_a: strategie A
-        :param strat_b: strategie B
-        :param condition: condition
+        Lit le capteur de distance (en mm).
+        :returns: entier distance en millimetre.
+            1. L'intervalle est de **5-8,000** millimeters.
+            2. Lorsque la valeur est en dehors de l'intervalle, le retour est **8190**.
         """
-        super().__init__()
-        self.adaptateur = adaptateur
-        self.pos_ini = None
-        self.parcouru = 0.
-        self.stratA = strat_a
-        self.stratB = strat_b
-
-        self.condition = condition
-        self.current_step = 0
-        print("hello")
-        self.logger = logging.getLogger(__name__)
-
-        # Initialise strat_a_faire à None
-        self.strat_a_faire = None
-
-    def start(self):
-        """commencer la strategie"""
-        self.logger.info("Starting condition strategy")
-        self.current_step = 0
-        print("hellos")
-        if self.adaptateur.get_distance >= self.condition:
-            self.strat_a_faire = self.stratA
-        else:
-            self.strat_a_faire = self.stratB
-        self.strat_a_faire.start()
-
-    def stop(self) -> bool:
-        """Verifier si la strategie est finie
-        :return: True si la strategie est finie, False sinon"""
-        # Check if strat_a_faire is None before accessing stop() method
-        if self.strat_a_faire is not None:
-            return self.strat_a_faire.stop()
-
-    def step(self):
-        """pas de la strategie sequentielle """
-        if self.stop():
-            return
+        #        return self.distanceSensor.read_range_single(False)
+        return self.robot.get_distance()
--- MVC/robot/robot2I013.py
+++ MVC/robot/robot2I013.py
@@ -1,73 +1,117 @@
-from MVC.controller.ai import Go, StrategieSequentielle, TournerDeg, StrategieIf, Stop, Strategie
+import math
+import threading
+from random import randint, random
 
 
-def test_if(controller) -> StrategieIf:
-    stop = Stop(controller.adaptateur)
-    strat_a = test_go_avec_tracer(controller)
-    strat_b = test_strat_seq_carre(controller)
+class Robot2IN013:
+    """
+    Classe d'encapsulation du robot et des senseurs.
+    Constantes disponibles :
+    LED (controle des LEDs) :  LED_LEFT_EYE, LED_RIGHT_EYE, LED_LEFT_BLINKER, LED_RIGHT_BLINKER, LED_WIFI
+    MOTEURS (gauche et droit) : MOTOR_LEFT, MOTOR_RIGHT
+    et les constantes ci-dessous qui definissent les elements physiques du robot
 
-    strat = StrategieIf(controller.adaptateur, strat_a, stop, 200)#, StrategieIf(controller.adaptateur, strat_b, stop, 200)]
+    """
 
-    return strat
+    WHEEL_BASE_WIDTH = 117  # distance (mm) de la roue gauche a la roue droite.
+    WHEEL_DIAMETER = 66.5  # diametre de la roue (mm)
+    WHEEL_BASE_CIRCUMFERENCE = WHEEL_BASE_WIDTH * math.pi  # perimetre du cercle de rotation (mm)
+    WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * math.pi  # perimetre de la roue (mm)
 
+    def __init__(self, nb_img=10, fps=25, resolution=None, servoPort="SERVO1", motionPort="AD1"):
+        """
+            Initialise le robot
+            :resolution: resolution de la camera
+            :servoPort: port du servo (SERVO1 ou SERVO2)
+            :motionPort: port pour l'accelerometre (AD1 ou AD2)
+        """
+        self._recording = False
+        self._thread = None
+        self.start_recording()
 
-def test(controller) -> Strategie:
-    """ Tester le tourner deg avec -45 deg puis 90 """
-    tracer_parcours = True
-    liste = [TournerDeg(controller.adaptateur, -45, 50, True),
-             Go(controller.adaptateur, 50, 50, 50, tracer_parcours),
-             TournerDeg(controller.adaptateur, 90, 50, tracer_parcours),
-             Go(controller.adaptateur, 50, 50, 50, tracer_parcours),
-             TournerDeg(controller.adaptateur, 90, 50, tracer_parcours),
-             Go(controller.adaptateur, 50, 50, 50, tracer_parcours),
-             TournerDeg(controller.adaptateur, 90, 50, tracer_parcours),
-             Go(controller.adaptateur, 50, 50, 50, tracer_parcours),
-             TournerDeg(controller.adaptateur, 90, 50, tracer_parcours),
-             Go(controller.adaptateur, 50, 50, 50, tracer_parcours),
-             TournerDeg(controller.adaptateur, 90, 50, tracer_parcours),
-             Go(controller.adaptateur, 50, 50, 50, tracer_parcours),
-             TournerDeg(controller.adaptateur, 90, 50, tracer_parcours),
-             Go(controller.adaptateur, 50, 50, 50, tracer_parcours),
-             TournerDeg(controller.adaptateur, 90, 50, tracer_parcours),
-             Go(controller.adaptateur, 50, 50, 50, tracer_parcours),
-             TournerDeg(controller.adaptateur, 90, 50, tracer_parcours)
-             ]
+    def stop(self):
+        """ Arrete le robot """
+        self.set_motor_dps("MOTOR_LEFT+MOTOR_RIGHT", 0)
+        ##print("STOP")
+        pass
 
-    strat = StrategieSequentielle(controller.adaptateur, liste)
-    return strat
+    def get_image(self):
+        pass
 
+    def get_images(self):
+        pass
 
-def test_avec_sans_tracer(controller) -> StrategieSequentielle:
-    liste = []
-    for i in range(4):
-        liste.append(Go(controller.adaptateur, 50, 50, 50, i % 2 == 0))
-        liste.append(TournerDeg(controller.adaptateur, 90, 50, True))
-    strat = StrategieSequentielle(controller.adaptateur, liste)
-    return strat
+    def set_motor_dps(self, port, dps):
+        """
+        Fixe la vitesse d'un moteur en nombre de degres par seconde
 
+        :port: une constante moteur,  MOTOR_LEFT ou MOTOR_RIGHT (ou les deux MOTOR_LEFT+MOTOR_RIGHT).
+        :dps: la vitesse cible en nombre de degres par seconde
+        """
+        print("set motor : ", port, dps)
+        pass
 
-def test_strat_seq_carre(controller) -> StrategieSequentielle:
-    tracer_parcours = True
-    steps = [Go(controller.adaptateur, 50, 50, 50, tracer_parcours),
-             TournerDeg(controller.adaptateur, 90, 50, tracer_parcours),
-             Go(controller.adaptateur, 50, 50, 50, tracer_parcours),
-             TournerDeg(controller.adaptateur, 90, 50, tracer_parcours),
-             Go(controller.adaptateur, 50, 50, 50, tracer_parcours),
-             TournerDeg(controller.adaptateur, 90, 50, tracer_parcours),
-             Go(controller.adaptateur, 50, 50, 50, tracer_parcours),
-             TournerDeg(controller.adaptateur, 90, 50, tracer_parcours)
-             ]
-    strat = StrategieSequentielle(controller.adaptateur, steps)
-    return strat
+    def get_motor_position(self):
+        """
+        Lit les etats des moteurs en degre.
+        :return: couple du  degre de rotation des moteurs
+        """
+        return (randint(0,180),randint(0,180))
 
 
-def test_go_sans_tracer(controller) -> Strategie:
-    tracer_parcours = False
-    strat = Go(controller.adaptateur, 50, 50, 50, tracer_parcours)
-    return strat
+    def offset_motor_encoder(self, port, offset):
+        """
+        Fixe l'offset des moteurs (en degres) (permet par exemple de reinitialiser a 0 l'etat
+        du moteur gauche avec offset_motor_encode(self.MOTOR_LEFT,self.read_encoders()[0])
 
+        :port: un des deux moteurs MOTOR_LEFT ou MOTOR_RIGHT (ou les deux avec +)
+        :offset: l'offset de decalage en degre.
 
-def test_go_avec_tracer(controller) -> Strategie:
-    tracer_parcours = True
-    strat = Go(controller.adaptateur, 50, 50, 50, tracer_parcours)
-    return strat
+        Zero the encoder by offsetting it by the current position
+        """
+        pass
+
+    def get_distance(self):
+        """
+        Lit le capteur de distance (en mm).
+        :returns: entier distance en millimetre.
+            1. L'intervalle est de **5-8,000** millimeters.
+            2. Lorsque la valeur est en dehors de l'intervalle, le retour est **8190**.
+        """
+        if random() > 0.5:
+            return 8190
+        return randint(5, 8000)
+
+    def servo_rotate(self, position):
+        """
+        Tourne le servo a l'angle en parametre.
+        :param int position: Angle de rotation, de **0** a **180** degres, 90 pour le milieu.
+        """
+        pass
+
+    def start_recording(self):
+        if self._recording or self._thread is not None:
+            self._stop_recording()
+        self._recording = True
+        self._thread = threading.Thread(target=self._start_recording)
+        self._thread.start()
+
+    def _stop_recording(self):
+        self._recording = False
+        self._thread.join()
+        self._thread = None
+
+    def _start_recording(self):
+        pass
+
+    def __getattr__(self, attr):
+        """ Méthodes héritées de GPG :
+        * set_led(self, led, red = 0, green = 0, blue = 0)
+            Allume une led.
+
+            :led: une des constantes LEDs (ou plusieurs combines avec +) : LED_LEFT_EYE, LED_RIGHT_EYE, LED_LEFT_BLINKER, LED_RIGHT_BLINKER, LED_WIFI.
+            :red: composante rouge (0-255)
+            :green:  composante verte (0-255)
+            :blue: composante bleu (0-255)
+        """
+        pass
